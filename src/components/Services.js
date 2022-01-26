55989b5da8ee7d5892af8bbec6932e4d354c5079
eric <61746940+M-erick@users.noreply.github.com>
2022-01-26 13:29:09 +0300

    updates Service.js href links

--- src/components/Services.js
+++ src/components/Services.js
@@ -10,7 +10,7 @@ const Services = () => {
                             <a className="max-w-2xl w-full block bg-white shadow-md rounded-md border-t-4 border-indigo-600 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110" href="#">
                                 <div className=" items-center px-4 py-2">
                                     <h3 className="text-lg font-medium text-gray-700 underline ">WEB DEVELOPMENT</h3>
-                                    <span className="block text-gray-600 font-light text-sm">HTML,CCS,PHP , REACT AND LARAVEL FRAMEWORK</span>
+                                    <span className="block text-gray-600 font-light text-sm">HTML,CCS,PHP ,MERN STACK,VUE, REACT AND LARAVEL FRAMEWORK</span>
                                 </div>
                             </a>
         
